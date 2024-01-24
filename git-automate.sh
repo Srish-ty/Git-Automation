3a601e247508b43043aa03d86256ae06bc782685
Srishty M <srishtymangutte@gmail.com>
2024-01-24 08:55:25 +0530

    changes

--- git-automate.sh
+++ git-automate.sh
@@ -7,6 +7,8 @@ read -p "Enter commit message: " commit_message
 read -p "Enter branch name (default is 'main'): " branch_name
 branch_name=${branch_name:-main}
 
+current_directory=$(pwd)
+
 # Run Git commands in the current working directory
 git add .
 echo -e '\033[0;32m1.Staged changes \n \033[0m'
@@ -15,4 +17,4 @@ git commit -m "$commit_message"
 echo -e '\033[0;35m2.Committed !\n \033[0m'
 git push origin $branch_name
 
-echo -e '\n \n\033[0;36mChanges commited and pushed successfully in to Origin from $(pwd).!! \n\033[0m'
+echo -e "\n \033[0;36mChanges committed and pushed successfully in ${current_directory}!\033[0m"
