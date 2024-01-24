22e92fc7b6333b667b7e21a11e6faa3f5f46326c
Srishty M <srishtymangutte@gmail.com>
2024-01-24 08:38:14 +0530

    feat: changes

--- git-automate.sh
+++ git-automate.sh
@@ -9,7 +9,10 @@ branch_name=${branch_name:-main}
 
 # Run Git commands in the current working directory
 git add .
+echo -e "\n1.Staged changes\n"
+
 git commit -m "$commit_message"
+echo -e "\n2.Committed !\n"
 git push origin $branch_name
 
-echo -e "\n \nChanges commited and pushed successfully in to Origin from $(pwd). \n"
+echo -e "\n \nChanges commited and pushed successfully in to Origin from $(pwd).!! \n"
