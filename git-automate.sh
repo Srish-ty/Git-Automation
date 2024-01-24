86694c515bd77f9f1c829b92afac108a74e134c3
Srishty M <srishtymangutte@gmail.com>
2024-01-24 23:45:21 +0530

    fix: changed text color

--- git-automate.sh
+++ git-automate.sh
@@ -11,11 +11,12 @@ current_directory=$(pwd)
 origin_url=$(git config --get remote.origin.url)
 
 # Run Git commands in the current working directory
+echo -e '\n'
 git add .
-echo -e '\033[0;32m1.Staged changes \n \033[0m'
+echo -e '\033[0;35m1.Staged all files \n \033[0m'
 
 git commit -m "$commit_message"
-echo -e '\033[0;35m2.Committed !\n \033[0m'
+echo -e '\033[0;32m2.Committed changes !\n \033[0m'
 git push origin $branch_name
 
-echo -e "\n \033[0;36mChanges committed and pushed successfully to \033[0;32m${origin_url}\033[0m branch \033[0;32m${branch_name}\033[0m from ${current_directory}!\033[0m\n\n\n\n"
+echo -e "\n\033[0;36mChanges committed and pushed successfully to \033[0;35m${origin_url}\033[0m \033[0;36mbranch \033[0;35m${branch_name}\033[0m \033[0;36mfrom ${current_directory}!\033[0m\n\n\n\n"
